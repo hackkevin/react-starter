f94c6ac77797a342985894ee0c80c20799c92448
unknown <v-kevwu@Lenovo-PC444.redmond.corp.microsoft.com>
2015-04-24 00:33:03 -0700

    some

--- build/.module-cache/8ca27c79bb1075c503af570ec823d2ee28a857fd.js
+++ build/.module-cache/8ca27c79bb1075c503af570ec823d2ee28a857fd.js
@@ -5,8 +5,7 @@ var CommentList = React.createClass({displayName: "CommentList",
    renter: function(){
        return (
            React.createElement("div", {className: "commentList"}, 
-               React.createElement(Comment, {author: "Pete Hunt"}, "This is one comment"), 
-               React.createElement(Comment, {author: "Jordan Walke"}, "This is *author* comment")
+               React.createElement(Comment, {author: "Pete Hunt"})
            )
        )
    }
--- src/tutorial5.js
+++ src/tutorial5.js
@@ -1,4 +1,14 @@
 /**
  * Created by v-kevwu on 4/24/2015.
  */
-var CommentList = React.c
+var CommentList = React.createClass({
+   renter: function(){
+       return (
+           <div className = "commentList">
+               <Comment author="Pete Hunt">This is one comment</Comment>
+               <Comment author="Jordan Walke">This is *author* comment</Comment>
+           </div>
+       )
+   }
+});
+
