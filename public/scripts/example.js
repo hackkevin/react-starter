b0024e83b81e3d83042a88eb8fb29089df27feab
mingnanwu <mingnanwu@gmial.com>
2015-04-25 15:34:08 -0700

    tutorial 18

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -61,21 +61,21 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
-    handleSumbit: function(e) {
+    handleSubmit: function(e) {
         e.preventDefault();
         var author = React.findDOMNode(this.refs.author).value.trim();
         var text = React.findDOMNode(this.refs.text).value.trim();
         if (!text || !author) {
             return;
         }
-        // TODO: send request to the server
+        this.props.onCommentSubmit({author: author, text: text});
         React.findDOMNode(this.refs.author).value = '';
         React.findDOMNode(this.refs.text).value = '';
-
+        return;
     },
     render: function() {
         return (
-            <form className="commentForm" onSumbit={this.handleSumbit}>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="Your name" ref="author" />
                 <input type="text" placeholder="Say something..." ref="text" />
                 <input type="submit" value="Post" />
