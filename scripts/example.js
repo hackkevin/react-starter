659c06bb54499ca58b958d7dd9c57fe3bf2fa6b8
mingnanwu <mingnanwu@gmial.com>
2015-04-25 00:26:30 -0700

    tutorial 13

--- scripts/example.js
+++ scripts/example.js
@@ -1,12 +1,31 @@
 /**
  * Created by mingnanwu on 4/24/15.
  */
-var CommentBox = React.createClass({displayName: 'CommentBox',
+var CommentBox = React.createClass({
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data} />
                 <CommentForm />
             </div>
         );
@@ -33,9 +52,11 @@ var CommentList = React.createClass({
 var CommentForm = React.createClass({
     render: function() {
         return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
+            <form className="commentForm">
+                <input type="text" placeholder="Your name" />
+                <input type="text" placeholder="Say something..." />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
@@ -61,6 +82,6 @@ var data = [
 ];
 
 React.render(
-    <CommentBox url= "comments.json" />,
+    <CommentBox url="comments.json" pollInterval={2000} />,
     document.getElementById('content')
 );
